1ce6d1b606b061f97ebad11126ee23dc591bd9c8
Gaurav <grvsgh7@gmail.com>
2021-11-22 21:42:02 +0530

    Operators: arthmatic and assignment

---
+++ _02Operators/arthmatic.js
@@ -0,0 +1,8 @@
+let a = 10, b = 3;
+
+console.log(a + b); //add
+console.log(a - b); //subtract
+console.log(a * b); //multiply
+console.log(a / b); //division
+console.log(a % b); //mod
+console.log(a** b); //power
---
+++ _02Operators/assignment.js
@@ -0,0 +1,20 @@
+var a = 10;
+console.log(a);
+
+a = 100;
+console.log(a);
+
+a += 10;
+console.log(a);
+
+a -= 10;
+console.log(a);
+
+a *= 10;
+console.log(a);
+
+a /= 10;
+console.log(a);
+
+a %= 10;
+console.log(a);
